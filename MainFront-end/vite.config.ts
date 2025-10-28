34d70593e9fae14cd7637d24fe6ae23f05e329b8
shahinshenavary <92166061+shahinshenavary@users.noreply.github.com>
2025-10-28 06:18:24 +0330

    Front end

---
+++ MainFront-end/vite.config.ts
@@ -0,0 +1,32 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  server: {
+    host: "127.0.0.1",
+    port: 8080,
+    fs: {
+      allow: ["./client", "./shared", path.resolve(__dirname, "node_modules")],
+      deny: [".env", ".env.*", "*.{crt,pem}", "**/.git/**"],
+    },
+    proxy: {
+      "/api": {
+        target: "https://localhost:7259",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+  build: {
+    outDir: "dist/spa",
+  },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./client"),
+      "@shared": path.resolve(__dirname, "./shared"),
+    },
+  },
+}));
